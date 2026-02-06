fe648bab09bc0d5e1369ae99bb383a930a60eb1b
Ben Brodeur <bab2661@rit.edu>
2026-02-05 23:03:07 -0500

    overseer fully setup

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,6 +1,22 @@
 return {
-  'stevearc/overseer.nvim',
-  ---@module 'overseer'
-  ---@type overseer.SetupOpts
+  "stevearc/overseer.nvim",
+  lazy = false, -- important for Telescope
   opts = {},
+  config = function(_, opts)
+    require("overseer").setup(opts)
+
+    vim.keymap.set(
+      "n",
+      "<leader>or",
+      "<cmd>OverseerRun<CR>",
+      { desc = "Overseer: Run task" }
+    )
+
+    vim.keymap.set(
+	"n",
+	"<leader>ot",
+	"<cmd>OverseerToggle<CR>",
+	{ desc = "Overseer: Toggle task list" }
+    )
+    end,
 }
